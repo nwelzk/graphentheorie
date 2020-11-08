cd2302945d15b5ada964d021f77316f6453fd070
Nils Welzk <nwelzk@gmail.com>
2020-11-08 21:19:22 +0100

    Beginn creating proof for task c

--- src/Proof.java
+++ src/Proof.java
@@ -1,49 +1,86 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Proof {
-	public Graph graph;
 	
-	public ArrayList<Node> nodes;
-	public ArrayList<Node> checkNodes;
+	public ArrayList<ArrayList<Integer>> bla;
+	private int laenge;
 	
-	public ArrayList<Node[]> inclMinRemovedNodes;
-	
-	public Proof(Graph g) {
-		this.graph = new Graph();
-		this.nodes = new ArrayList<Node>();
-		this.checkNodes = new ArrayList<Node>();
-		this.inclMinRemovedNodes= new ArrayList<Node[]>(); 
+	public Proof(int laenge) {
+//		aufgabe_a();
+//		aufgabe_b();
+//		aufgabe_c();
 		
-		this.graph = g.clone();
-		this.nodes.addAll(graph.nodes);
-		this.checkNodes.addAll(this.nodes);
+		this.laenge = laenge;
+		this.bla = new ArrayList<ArrayList<Integer>>();
+		// Füllt eine Liste mit Listen, in denen der Wurzelknoten für den baum steckt.
 		
-		// Alle Einzelknoten	
-		for (Node node : this.checkNodes) {
-			this.proceed(node);
+		int[] gesamt = {0,1,2,3};
+		
+		for (int node : gesamt) {
+			ArrayList<Integer> start = new ArrayList<Integer>();
+			start.add(node);
+			this.routine(gesamt, start);
 		}
-		
-		this.checkNodes.clear();
-		this.checkNodes.addAll(this.nodes);
-		
-	}
+
+	}	
 	
-	
-	public void proceed(Node[] s) {
-		if(this.graph.checkGraphBreakIntoMoreOddSizedComponentsAsGivenRemoteNodes(s)) {
-			this.inclMinRemovedNodes.add(s);
-			
-			for (Node n : s) {
-				if (this.nodes.contains(n)) {
-					this.nodes.remove(n);
+	public void routine(int[] gesamt, ArrayList<Integer> start) {
+		
+		for (int node : gesamt) {	
+			if (start.get(start.size()-1) < node) {
+				ArrayList<Integer> newList = new ArrayList<Integer>();
+				newList.addAll(start);
+				// Reihe ist aufsteigend
+				newList.add(node);
+				if (newList.size() == this.laenge) {
+					if (! this.bla.contains(newList)) {
+						this.bla.add(newList);
+					}
+				}
+				else {
+					this.routine(gesamt, newList);
 				}
 			}
 		}
 	}
-	public void proceed(Node node) {
-		Node[] c = new Node[1];
-		c[0] = node;
-		this.proceed(c);
+	public void printBla() {
+		for (ArrayList<Integer> arrayList : this.bla) {
+			System.out.println(Arrays.toString(arrayList.toArray()));
+		}
 	}
 	
 }
+	
+//	public ArrayList<Node[]> proceedCombinations(int currentLevel, int maxLevel, ArrayList<Node> nodes, ArrayList<Node[]> lists) {
+//		ArrayList<Node[]> newLists = new ArrayList<Node[]>();
+//		
+//		if(currentLevel >= maxLevel) {
+//			return lists;
+//		}
+//		
+//		for (Node[] nodesArray : lists) {
+//			
+//		}
+//		ArrayList<Node[]> newLists = proceedCombinations(currentLevel + 1, maxLevel, nodes, lists);
+//		
+//		
+//	}
+//	
+//	
+//	public void proceed(Node[] s) {
+//		if(this.graph.checkGraphBreakIntoMoreOddSizedComponentsAsGivenRemoteNodes(s)) {
+//			this.inclMinRemovedNodes.add(s);
+//			
+//			for (Node n : s) {
+//				if (this.nodes.contains(n)) {
+//					this.nodes.remove(n);
+//				}
+//			}
+//		}
+//	}
+//	public void proceed(Node node) {
+//		Node[] c = new Node[1];
+//		c[0] = node;
+//		this.proceed(c);
+//	}
--- src/_Main.java
+++ src/_Main.java
@@ -1,23 +1,18 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Random;
 import java.io.*;
 
 public class _Main {
 	
+	ArrayList<ArrayList<Integer>> bla;
+	
 	public static void main(String[] args) {
-//		aufgabe_a();
-//		aufgabe_b();
-//		aufgabe_c();
-		
-		int[] gesamt = {0,1,2,3,4};
-//		ArrayList<int[]> bla = getKombinations(gesamt, 2);
-		
-//		for (int aa = 1; aa <= gesamt.length; aa++) { //bis zur maximallänge der kette
-//			ArrayList<int[]> kombinationen = getKombinations();
-//			
-//		}
-	}	
+		Proof p = new Proof(3);
+		p.printBla();
+	}
+	
 	
 	public static void check() {
 		Graph g = new Graph("[[10], [5, 8], [13], [5], [5, 7], [1, 3, 4, 10, 13], [11, 13], [4], [1], [10, 13], [0, 5, 9], [6, 12], [11], [2, 5, 6, 9]]");
@@ -60,7 +55,6 @@ public class _Main {
 		
 		System.out.println("Fertig!");
 	}
-
 	public static void aufgabe_b() {
 		int sumGraphs = 0;
 		Random r = new Random();
@@ -113,28 +107,6 @@ public class _Main {
 		}		
 		System.out.println("Anzahl der Kontellationen: " + sumGraphs);
 		System.out.println("Fertig!");
-	}
-
-	public static void aufgabe_c() {
-		
- 
-		
-//		
-//		for (int ii = 1; ii < g.nodes.size(); ii++) { // Anzahl der entfernten Ecken;
-//			for (int jj = 1; jj <= ii; jj++) {
-//				ArrayList<Integer> arr = new ArrayList<Integer>();
-//				
-//				for (int aa = 0; aa < g.nodes.size(); aa++) {
-//					//
-//				}
-//				
-////					arr.add(node.index);
-////					System.out.println(node.index);
-//			}
-////				if(g.checkGraphBreakIntoMoreOddSizedComponentsAsivenRemoteNodes(arr)) {
-////					System.out.println(arr.toString());
-////				}
-//		}		
 	}
 
 //	Graph g = new Graph("[[1], [0, 2], [1, 3, 5, 6], [2, 4], [3, 5], [2, 4], [2, 7], [6, 8], [7, 9], [8]]"); // nicht paarungsunperfekt
